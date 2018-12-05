416c9e39e2160b9a3985eabbb77879e53f6ac626
Aaron Grodzinsky <aarongrodzinsky@gmail.com>
2018-12-05 14:51:48 -0500

    Update comments

--- pacmanTask.py
+++ pacmanTask.py
@@ -2,30 +2,31 @@ from pybrain.rl.environments.task import Task
 from runPacman import RunPacman
 import pygame
 
+#*****************************************************************#
+# Pac-Man Task Class                                              #
+# This is the Pac-Man Task Class, a child of PyBrain's Task Class #
+# It holds the agent's actions, rewards, and interaction with the #
+# environment.                                                    #     
+#*****************************************************************#
+
 class PacmanTask(Task):
     
+    # This initializes the Task that the Pac-Man agent will be completing
     def __init__(self, environment, game):
         self.env = environment
         self.game = game
-        #self.gameReference = RunPacman(environment)
-        #self.gameReference.executeGame()
         
+    # This calls to the environment to return the needed information into the PyBrain Q-Learning library   
     def getObservation(self):
         return self.env.getSensors()
         
-    def performAction(self, action):
-        #self.env.resetMap()
-        #print("PACMAN TAKE ACTION", action)
-        
-        #action = int(action.tolist()[0])
+    # This is called by PyBrain to give the Pac-Man a certain action to take in the environment
+    # It represents a direction for the agent to move in the maze
+    def performAction(self, action):        
         action = self.game.getValidMove(int(action.tolist()[0]))
-        #print("MOVING: ",action)
         self.game.executeMove(action)
         
-        #self.gameReference = RunPacman(self.env)
-        #self.gameReference.executeGame()
-        #print("")
-        
+    # This function calls the game's function to give a reward to the agent based on its previous action    
     def getReward(self):
         return self.game.getReward()
         
